c43b6e4c1446ceda63bac0cc0f31e3970159e933
Christopher League <league@contrapunctus.net>
2012-03-22 10:42:08 -0400

    Pass selectlines test.

--- filter.hs
+++ filter.hs
@@ -1,13 +1,25 @@
 import Text.Pandoc
 
-main = toJsonFilter include
+main = toJsonFilter processBlock
 
-include :: Block -> IO Block
-include (cb@(CodeBlock (i,cs,kvs) _)) =
+processBlock :: Block -> IO Block
+processBlock (cb@(CodeBlock (i,cs,kvs) _)) =
   case lookup "include" kvs of
-    Just f -> readFile f >>= return . CodeBlock (i,cs,kvs) . chop
+    Just f -> readFile f >>= return . CodeBlock (i,cs,kvs) . rewrite kvs
     Nothing -> return cb
-include b = return b
+processBlock b = return b
+
+rewrite :: [(String,String)] -> String -> String
+rewrite kvs = chop . unlines . doFirst . doLast . lines
+  where doFirst = maybeLinesF "first" (drop . (flip (-) 1) . read) kvs
+        doLast = maybeLinesF "last" (take . read) kvs
+
+maybeLinesF :: String -> (String -> [String] -> [String])
+    -> [(String,String)] -> [String] -> [String]
+maybeLinesF key f kvs xs =
+  case lookup key kvs of
+    Just v -> f v xs
+    Nothing -> xs
 
 chop :: String -> String
 chop s =
