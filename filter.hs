2a465bd08fedbaa2fb73c06843da7c03a7002d4d
Christopher League <league@contrapunctus.net>
2012-03-22 12:02:01 -0400

    Code to pass prompt test.

--- filter.hs
+++ filter.hs
@@ -1,17 +1,25 @@
 import Text.Pandoc
 import Data.List (isInfixOf)
+import Text.Regex.Posix
+import Web.Encodings (encodeHtml)
 
 main = toJsonFilter processBlock
 
 processBlock :: Block -> IO Block
-processBlock (cb@(CodeBlock (i,cs,kvs) _)) =
+processBlock (cb@(CodeBlock (i,cs,kvs) body)) =
   case lookup "include" kvs of
     Just f -> readFile f >>= return . CodeBlock (i,cs,kvs) . rewrite kvs
-    Nothing -> return cb
+    Nothing ->
+      case lookup "prompt" kvs of
+        Just regex -> return $ RawBlock "html" $ "<pre class=\"sourceCode\">" ++ (asLines (map $ highlightCmd regex) (encodeHtml body)) ++ "</pre>\n"
+        Nothing -> return cb
 processBlock b = return b
 
+asLines :: ([String] -> [String]) -> String -> String
+asLines f = unlines . f . lines
+
 rewrite :: [(String,String)] -> String -> String
-rewrite kvs = chop . unlines . doMarkers . doFirst . doLast . lines
+rewrite kvs = chop . asLines (doMarkers . doFirst . doLast)
   where
     doMarkers = maybeMarkers kvs
     doLast = maybeLinesF "last" (take . read) kvs
@@ -41,3 +49,13 @@ chop s =
   case reverse s of
     '\n' : rs -> reverse rs
     _ -> s
+
+highlightCmd :: String -> String -> String
+highlightCmd regex s =
+  case s =~ regex of
+    (before, "", "") -> before
+    (before, match, "") ->
+      before ++ "<span class=\"prompt\">" ++ match ++ "</span>"
+    (before, match, after) ->
+      before ++ "<span class=\"prompt\">" ++ match ++ "</span>" ++
+        "<span class=\"cmd\">" ++ after ++ "</span>"
