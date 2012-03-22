34d31cffeaa61b4179c7cf45e61f40415b327f9e
Christopher League <league@contrapunctus.net>
2012-03-21 21:57:07 -0400

    Pass minimal inclusion test.

--- filter.hs
+++ filter.hs
@@ -1,5 +1,16 @@
 import Text.Pandoc
 
-main = toJsonFilter f
-  where f :: Block -> IO Block
-        f = return
+main = toJsonFilter include
+
+include :: Block -> IO Block
+include (cb@(CodeBlock (i,cs,kvs) _)) =
+  case lookup "include" kvs of
+    Just f -> readFile f >>= return . CodeBlock (i,cs,kvs) . chop
+    Nothing -> return cb
+include b = return b
+
+chop :: String -> String
+chop s =
+  case reverse s of
+    '\n' : rs -> reverse rs
+    _ -> s
